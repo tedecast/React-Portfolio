df3e66adad5972888956a8012441b9aa69f61e34
Teresa de Castro <decastro.ter@gmail.com>
2023-05-24 17:59:15 -0400

    Added in more icons

--- src/components/Layout/Sidebar/index.js
+++ src/components/Layout/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../../assets/images/logo-s.png'
 import LogoSubtitle from '../../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -35,6 +36,38 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
+    <ul>
+      {/* LinkedIn */}
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/teresa-de-castro/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      {/* Github */}
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/tedecast"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      {/* YouTube  */}
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.youtube.com/channel/UC6QupMa6A8uoxWU3Mq0ZyKA"
+        >
+          <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
