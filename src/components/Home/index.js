c330bdcf32f92e27213cee8c37d4f88ec103b1f5
Teresa de Castro <decastro.ter@gmail.com>
2023-05-26 16:53:24 -0400

    Got the animation to work

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,38 +1,67 @@
-import LogoTitle from '../../assets/images/logo-s.png';
-import { Link } from 'react-router-dom';
-import './index.scss';
-import AnimatedLetters from '../AnimatedLetters';
-import { useState } from 'react';
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
 
 const Home = () => {
-    const [letterClass, setLetterClass] = useState('text-animate');
-    const nameArray =  ['l', 'o', 'b', 'o', 'd', 'a', 'n'];
-    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l','o','p','e','r', '.'];
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
 
-    return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                <span className={letterClass}>H</span> 
-                <span className={`${letterClass} _12`}>i,</span> 
-                <br />
-                <span className={`${letterClass} _13`}>I</span> 
-                <span className={`${letterClass} _14`}>'m</span> 
-                <img src={LogoTitle} alt="developer" />
-                <AnimatedLetters letterClass={letterClass} 
-                strArray={nameArray}
-                idx={15}/>
-                <br />
-                <AnimatedLetters letterClass={letterClass} 
-                strArray={jobArray}
-                idx={22}/>
-                </h1>
-                <h2>Front Developer / JavaScript Expert / Youtuber</h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
-            </div>
-        </div>
-    );
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000);
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, []);
 
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>Front Developer / JavaScript Expert / Youtuber</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
