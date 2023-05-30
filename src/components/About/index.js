ed5f10f345cba6324b87c71485d176e46bacdf96
Teresa de Castro <decastro.ter@gmail.com>
2023-05-30 15:42:54 -0400

    Made chages to h1 closing tag

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,25 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
+import { useState, useEffect } from 'react'
 
 const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [])
+
   return (
     <div className="container about-page">
       <div className="text-zone">
         <h1>
           <AnimatedLetters
+            letterClass={letterClass}
             strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
             idx={15}
           />
