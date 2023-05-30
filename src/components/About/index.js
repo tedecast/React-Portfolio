925401c78bad255a0ef3b3c03bcb806004f7f2e5
Teresa de Castro <decastro.ter@gmail.com>
2023-05-30 16:02:42 -0400

    Updated logo colours

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,15 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useState, useEffect } from 'react'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -38,6 +47,29 @@ const About = () => {
           person, father of a beautiful daughter, a sports fanatic, photography
           enthusiast, and tech-obsessed!!!
         </p>
+      </div>
+
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+          </div>
+        </div>
       </div>
     </div>
   )
