86afe8b9aa2d835cddbd1d08af06a5f649098e89
Teresa de Castro <decastro.ter@gmail.com>
2023-05-31 10:38:08 -0400

    Added in contact page title

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,36 @@
-import './index.scss';
+import Loader from 'react-loaders'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
 
-const Contact = () => 'Hello';
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-export default Contact;
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [])
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+            letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+        </div>
+        <Loader type="pacman" />
+      </div>
+    </>
+  )
+}
+
+export default Contact
