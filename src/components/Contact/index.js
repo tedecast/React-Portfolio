6c19ada876a9e9f5b49fef54f8913461efee5c6f
Teresa de Castro <decastro.ter@gmail.com>
2023-05-31 12:27:40 -0400

    Got the map to work

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,6 +2,7 @@ import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -78,6 +79,14 @@ const Contact = () => {
             Sremeska Mitrovica <br />
             {/* Town ^ */}
             <span>freelancerslobodan@gmail.com</span>
+        </div>
+        <div className='map-wrap'>
+            <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <Marker position={[44.96366, 19.61045]}>
+                    <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
+                </Marker>
+            </MapContainer>
         </div>
         <Loader type="pacman" />
       </div>
