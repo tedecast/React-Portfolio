476c56a77727ea71fab14ea86e3010e22ff3a0b4
Teresa de Castro <decastro.ter@gmail.com>
2023-05-31 11:08:54 -0400

    Adjusting contact inputs

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -21,11 +21,52 @@ const Contact = () => {
         <div className="text-zone">
           <h1>
             <AnimatedLetters
-            letterClass={letterClass}
+              letterClass={letterClass}
               strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
               idx={15}
             />
           </h1>
+          <p>
+            I am interested in freelance opportunities - especially ambitious or
+            large projects. However, if you have other requests or questions,
+            please don't hesitate to contact me using the below form either.
+          </p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                    <input type="submit" className="flat-button" value="SEND"/>
+
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
         <Loader type="pacman" />
       </div>
